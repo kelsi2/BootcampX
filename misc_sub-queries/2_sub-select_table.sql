0142cf694be268c9091291522416b5698678665c
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-09-15 19:53:16 +0000

    add teachers and assistance requests tables and data

---
+++ misc_sub-queries/2_sub-select_table.sql
@@ -0,0 +1,14 @@
+-- SELECT *
+-- FROM (
+--   SELECT something_id
+--   FROM someTable
+--   WHERE something
+-- ) as sub_table;
+
+SELECT avg(total_students) as average_students
+FROM (
+  SELECT count(students) as total_students, cohorts.name as cohort_name
+  FROM students
+    JOIN cohorts on cohorts.id = cohort_id
+  GROUP BY cohorts.name
+) as totals_table;
---
+++ misc_sub-queries/3_in.sql
@@ -0,0 +1,17 @@
+-- SELECT *
+-- FROM table
+-- WHERE id IN (
+--   SELECT something_id
+-- FROM someTable
+-- WHERE something
+-- );
+
+SELECT assignments.name
+FROM assignments
+WHERE id NOT IN
+(
+  SELECT assignment_id
+FROM assignment_submissions
+  JOIN students ON students.id = student_id
+WHERE students.name = 'Ibrahim Schimmel'
+);

5365670348c2c5c63fb58bcefb68ed52c52b2bdf
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-09-16 21:10:17 +0000

    add bootcamp_app

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,19 @@
+const {Pool} = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: 123,
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+let cohortName = process.argv[2];
+
+pool.query('SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort FROM teachers JOIN assistance_requests ON teachers.id = teacher_id JOIN students ON students.id = student_id JOIN cohorts ON cohorts.id = cohort_id WHERE cohorts.name = $1 ORDER BY teacher;', [cohortName])
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.cohort}: ${user.teacher}`);
+    });
+    pool.end();
+  })
+  .catch(err => console.error('query error', err.stack));
